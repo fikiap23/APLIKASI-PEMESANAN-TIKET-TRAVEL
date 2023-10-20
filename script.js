8df8d4d09b923940746f928594fa83fccfce9260
fikiaprian23 <fikiaprian23@gmail.com>
2023-10-20 17:40:10 +0700

    [FIX] diskon & harga

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 function hitungTotal() {
-  var harga = parseFloat(document.getElementById('harga').value)
-  var jumlahTiket = parseInt(document.getElementById('jumlahTiket').value)
-  var isMember = document.getElementById('member').checked
-  var diskon = parseFloat(document.getElementById('diskon').value) || 0
+  let harga = parseFloat(document.getElementById('harga').value)
+  let jumlahTiket = parseInt(document.getElementById('jumlahTiket').value)
+  let isMember = document.getElementById('member').checked
+  let diskon = parseFloat(document.getElementById('diskon').value) || 0
+  let tujuan = document.getElementById('tujuan').value
+  console.log(tujuan)
 
-  var total = harga * jumlahTiket
+  let total = harga * jumlahTiket
 
   if (isMember) {
     total -= (total * diskon) / 100
   }
 
-  var totalBayarField = document.getElementById('totalBayarField')
-  var totalBayar = document.getElementById('totalBayar')
+  let totalBayarField = document.getElementById('totalBayarField')
+  let totalBayar = document.getElementById('totalBayar')
   totalBayar.value = 'Rp. ' + total
   totalBayarField.style.display = 'block'
   totalBayarField.style.visibility = 'visible'
 }
 
 function showDiskon() {
-  var isMember = document.getElementById('member').checked
-  var diskonField = document.getElementById('diskonField')
+  let isMember = document.getElementById('member').checked
+  let diskonField = document.getElementById('diskonField')
+  let diskon = document.getElementById('diskon')
 
   if (isMember) {
     diskonField.style.display = 'block'
     diskonField.style.visibility = 'visible'
+    diskon.value = 20
+    console.log(diskon)
   } else {
     diskonField.style.display = 'none'
     diskonField.style.visibility = 'hidden'
+  }
+}
+
+function showHarga() {
+  let tujuan = document.getElementById('tujuan').value
+  let harga = document.getElementById('harga')
+  console.log(tujuan)
+
+  if (tujuan === 'Jakarta') {
+    harga.value = 20000
+  } else if (tujuan === 'Surabaya') {
+    harga.value = 15000
+  } else if (tujuan === 'Bandung') {
+    harga.value = 10000
+  } else if (tujuan === 'Yogyakarta') {
+    harga.value = 25000
   }
 }
